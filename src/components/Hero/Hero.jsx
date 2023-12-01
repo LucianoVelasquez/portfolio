c8c774829e055b870de16398d6da32f5d3358389
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-12-01 07:37:29 -0300

    fixed avatar2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
   return (
     <div className="hero min-h-screen  bg-base-200 z-20 herores">
       <div className="hero-content flex-col lg:flex-row-reverse z-10 ">
-        <img  variants={textVariants} src="./src/components/hero/avatar3.png" className="object-contain w-12 rounded-full xl:w-1/2  xl:mb-28 lg:w-80 lg:mb-28 imgres" />
+        <img  variants={textVariants} src="https://raw.githubusercontent.com/LucianoVelasquez/portfolio/main/src/components/Hero/avatar3.png" className="object-contain w-12 rounded-full xl:w-1/2  xl:mb-28 lg:w-80 lg:mb-28 imgres" />
         <motion.div className="mr-16 containerres" variants={textVariants} initial="initial" animate="animate">
           <motion.h1 variants={textVariants} className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-red-500 to-yellow-300  font-bold tracking-wider">LUCIANO VELASQUEZ</motion.h1>
           <motion.h2 variants={textVariants} className="text-5xl font-bold mt-6 text-start">Full Stack Developer</motion.h2>
